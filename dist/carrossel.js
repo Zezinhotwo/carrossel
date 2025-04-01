ae1fe2065d2196ef108d20e08d535d57f7d84d9c
zezinhotwo <107373132+Zezinhotwo@users.noreply.github.com>
2025-03-31 21:53:44 -0300

    animaçao e logica de eventos js

--- dist/carrossel.js
+++ dist/carrossel.js
@@ -1,8 +1,19 @@
 export default class Carrossel {
     constructor() {
+        this.back = document.querySelector(".bntBack");
+        this.next = document.querySelector(".bntNext");
         this.init();
     }
     init() {
-        alert("ola");
+        var _a, _b;
+        const carrossel = document.querySelector(".carrossel-items");
+        (_a = this.next) === null || _a === void 0 ? void 0 : _a.addEventListener("click", (event) => {
+            carrossel === null || carrossel === void 0 ? void 0 : carrossel.classList.remove("back");
+            carrossel === null || carrossel === void 0 ? void 0 : carrossel.classList.toggle("next");
+        });
+        (_b = this.back) === null || _b === void 0 ? void 0 : _b.addEventListener("click", (event) => {
+            carrossel === null || carrossel === void 0 ? void 0 : carrossel.classList.remove("next");
+            carrossel === null || carrossel === void 0 ? void 0 : carrossel.classList.toggle("back");
+        });
     }
 }
--- src/carrossel/carrossel.ts
+++ src/carrossel/carrossel.ts
@@ -1,9 +1,21 @@
 export default class Carrossel {
+  private next: HTMLDivElement | null;
+  private back: HTMLButtonElement |null;
   constructor() {
+    this.back = document.querySelector(".bntBack");
+    this.next = document.querySelector(".bntNext");
     this.init();
   }
 
   init(): void {
-    alert("ola");
+    const carrossel = document.querySelector(".carrossel-items") 
+    this.next?.addEventListener("click", (event) => {
+      carrossel?.classList.remove("back");
+      carrossel?.classList.toggle("next");
+    });
+    this.back?.addEventListener("click", (event) => {
+      carrossel?.classList.remove("next");
+      carrossel?.classList.toggle("back");
+    });
   }
 }
